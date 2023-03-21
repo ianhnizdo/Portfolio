1d7247fb61d52558ec6d0198140a86829c1ca9cc
ian <ianr.hnizdo@gmail.com>
2023-03-21 18:16:40 -0400

    Created base coimponents.

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,28 +1,19 @@
 import react from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Homepage from './components/HomePage'
 
 export default function App() {
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Homepage />
+        }
+    ])
+
     return(
         <section className="App">
-            
-            <article className="About">
-            
-                <figure className="figure-container">
-            
-                <img alt="A picture of myself, Ian Hnizdo"/>
-            
-                <figcaption>
-
-                </figcaption>
-                
-                </figure>
-            
-            </article>
-
-            <section>
-
-            </section>
-
-            
+                        
         </section>
     )
 }
---
+++ my-portfolio/src/components/Contact.tsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+export default function Contact(){
+    return(
+        <section className="contact">
+
+            
+
+        </section>
+    )
+}
